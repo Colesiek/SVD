c79a1c1f54f3709e145b1ddd732b99432c53b02d
Colesiek <colesiek@gmail.com>
2012-11-13 21:38:32 +0100

    DONE

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.richTextBox4 = new System.Windows.Forms.RichTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(415, 287);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(67, 26);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Działaj";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(415, 261);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(111, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(34, 24);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(286, 179);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(34, 209);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(286, 179);
+            this.richTextBox2.TabIndex = 3;
+            this.richTextBox2.Text = "";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(34, 394);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(286, 38);
+            this.richTextBox3.TabIndex = 4;
+            this.richTextBox3.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(15, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "U";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 212);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(21, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "VT";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 394);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(18, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "W";
+            // 
+            // richTextBox4
+            // 
+            this.richTextBox4.Location = new System.Drawing.Point(359, 24);
+            this.richTextBox4.Name = "richTextBox4";
+            this.richTextBox4.Size = new System.Drawing.Size(223, 179);
+            this.richTextBox4.TabIndex = 8;
+            this.richTextBox4.Text = "";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(356, 8);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "SVDprzed";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(595, 437);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.richTextBox4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox richTextBox4;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+
+            double[,] a1;
+            double[] w;
+            double[,] u;
+            double[,] vt;
+            double[,] przed = new double[9,9];
+            double[] dlugosci = new double[9];
+            double[,] odleglosci = new double[9,9];
+            double[,] iloczyny = new double[9, 9];
+            static double [,] a = new double[,] { {1, 0, 0, 1, 0, 0, 0, 0, 0 }, { 1, 0, 1, 0, 0, 0, 0, 0, 0 }, { 1, 1, 0, 0, 0, 0, 0, 0, 0 }, { 0, 1, 1, 0, 1, 0, 0, 0, 0 }, { 0, 1, 1, 2, 0, 0, 0, 0, 0 }, { 0, 1, 0, 0, 1, 0, 0, 0, 0 }, { 0, 1, 0, 0, 1, 0, 0, 0, 0 }, { 0, 0, 1, 1, 0, 0, 0, 0, 0 }, { 0, 1, 0, 0, 0, 0, 0, 0, 1  }, { 0, 0, 0, 0, 0, 1, 1, 1, 0 }, { 0, 0, 0, 0, 0, 0, 1, 1, 1  }, { 0, 0, 0, 0, 0, 0, 0, 1, 1 } };
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void norm()
+        {
+            a1 = a;
+            for (int i = 0; i < 9; i++)
+            {
+                double suma = 0;
+                for (int j = 0; j < 12; j++)
+                {
+                    suma += a1[j, i];
+                }
+                for (int j = 0; j < 12; j++)
+                {
+                    a1[j, i] = a1[j, i] - (suma / 9);
+                }
+            }
+        }
+
+        void mnozenie()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = i + 1; j < 9; j++)
+                {
+                    przed[i, j] = iloczyny[i, j] / dlugosci[i] * dlugosci[j];
+                }
+            }
+        }
+
+        void dlugosc()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 12; j++)
+                {
+                    dlugosci[i] += Math.Pow(a1[j, i], 2);
+                }
+                dlugosci[i] += Math.Sqrt(dlugosci[i]);
+            }
+        }
+
+        void iloczyn()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = i + 1; j < 9; j++)
+                {
+                    for (int k = 0; k < 12; k++)
+                    {
+                        iloczyny[i, j] += a1[k, i] * a1[k, j];
+                    }
+                }
+            }
+        }
+
+        void odl()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    odleglosci[i, j] = dlugosci[i] - dlugosci[j];
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            alglib.rmatrixsvd(a, 12, 9, 2, 2, 0, out w, out u, out vt);
+
+
+            richTextBox1.Text = "";
+            richTextBox2.Text = "";
+            richTextBox3.Text = "";
+            richTextBox4.Text = "";
+
+
+            //druk U
+            for(int i=0; i<12; i++)
+            {
+                double t = 0;
+                for(int j=0; j<9; j++)
+                {
+                    t = Math.Round(u[j, i], 2);
+                    richTextBox1.Text += t.ToString();
+                    richTextBox1.Text += " ";
+                }
+                richTextBox1.Text += "\n";
+            }
+
+            //druk VT
+            for (int i = 0; i < 9; i++)
+            {
+                double t = 0;
+                for (int j = 0; j < 9; j++)
+                {
+                    t = Math.Round(vt[i, j], 2);
+                    richTextBox2.Text += t.ToString();
+                    richTextBox2.Text += " ";
+                }
+                richTextBox2.Text += "\n";
+            }
+
+            //druk W
+                for (int j = 0; j < 9; j++)
+                {
+                    double t = 0;
+                    t = Math.Round(w[j], 2);
+                    richTextBox3.Text += t.ToString();
+                    richTextBox3.Text += " ";
+                }
+
+
+                norm();
+                iloczyn();
+                dlugosc();
+                mnozenie();
+
+                
+                    for (int i = 0; i < 9; i++)
+                    {
+                        double t = 0;
+                        for (int j = 0; j < 9; j++)
+                        {
+                            t = Math.Round(przed[j, i], 2);
+                            if (t == 0)
+                                richTextBox4.Text += "  ";
+                            else
+                            {
+                                richTextBox4.Text += t.ToString();
+                                richTextBox4.Text += " ";
+                            }
+                        }
+                        richTextBox4.Text += "\n";
+                    }
+        }
+    }
+}
